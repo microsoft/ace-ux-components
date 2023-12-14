e9851859249f54265f1a64ff2554e10e73d3df1a
Kyle Mas <kylemas@microsoft.com>
2023-12-14 12:56:27 -0800

    add tsignore for build issues on dashboard

--- dashboard/src/adaptiveCardExtensions/sampleComponents/SampleComponentsAdaptiveCardExtension.ts
+++ dashboard/src/adaptiveCardExtensions/sampleComponents/SampleComponentsAdaptiveCardExtension.ts
@@ -53,88 +53,110 @@ export default class SampleComponentsAdaptiveCardExtension extends BaseAdaptiveC
   private registerQuickViews() {
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_COMPONENTS_QUICK_VIEW,
+      // @ts-ignore
       () => new SampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_ALERT_BOX_VIEW,
+      // @ts-ignore
       () => new AlertBoxSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_ALERT_BOX_SUCCESS_VIEW,
+      // @ts-ignore
       () => new AlertBoxSuccessSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_ALERT_BOX_ERROR_VIEW,
+      // @ts-ignore
       () => new AlertBoxErrorSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_ALERT_BOX_WARNING_VIEW,
+      // @ts-ignore
       () => new AlertBoxWarningSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_ALERT_BOX_INFO_VIEW,
+      // @ts-ignore
       () => new AlertBoxInfoSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_CHOICE_SET_VIEW,
+      // @ts-ignore
       () => new ChoiceSetSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_EMPTY_VIEW,
+      // @ts-ignore
       () => new EmptySampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_ERROR_VIEW,
+      // @ts-ignore
       () => new ErrorSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_HEADER_VIEW,
+      // @ts-ignore
       () => new HeaderSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_INPUT_DATE_VIEW,
+      // @ts-ignore
       () => new InputDateSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_INPUT_NUMBER_VIEW,
+      // @ts-ignore
       () => new InputNumberSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_INPUT_TIME_VIEW,
+      // @ts-ignore
       () => new InputTimeSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_INPUT_TOGGLE_VIEW,
+      // @ts-ignore
       () => new InputToggleSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_ITEMS_LIST_VIEW,
+      // @ts-ignore
       () => new ItemsListSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_LOADING_INDICATOR_VIEW,
+      // @ts-ignore
       () => new LoadingIndicatorSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_PERSONA_VIEW,
+      // @ts-ignore
       () => new PersonaSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_PICKER_VIEW,
+      // @ts-ignore
       () => new PickerSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_SEARCH_VIEW,
+      // @ts-ignore
       () => new SearchSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_SUCCESS_VIEW,
+      // @ts-ignore
       () => new SuccessSampleQuickView(this.context)
     );
     this.quickViewNavigator.register(
       SampleComponentIds.SAMPLE_TOGGLE_ROW_VIEW,
+      // @ts-ignore
       () => new ToggleRowSampleQuickView(this.context)
     );
+    // @ts-ignore
     this.quickViewNavigator.register(SampleComponentIds.ICON_LIST_VIEW, () => new IconListView(this.context));
   }
 
