b64bee9aa4fb0a2d3a684c45dee89ef01a7216c7
Kyle Mas <kylemas@microsoft.com>
2024-11-21 09:44:43 -0800

    update getTheme method async

--- ux-components/src/getTheme.ts
+++ ux-components/src/getTheme.ts
@@ -1,11 +1,14 @@
 import { AdaptiveCardExtensionContext } from "@microsoft/sp-adaptive-card-extension-base";
 import { HostTheme } from "./types";
 
-export function getTheme(context: AdaptiveCardExtensionContext): HostTheme {
-    // Sets the default theme to light theme
-    let theme: HostTheme = "light";
-    context.sdks?.microsoftTeams?.teamsJs?.app?.getContext().then((context) => {
-      theme = context.app?.appInfo?.theme as HostTheme;
-    });
-    return theme;
+export async function getTheme(context: AdaptiveCardExtensionContext): Promise<HostTheme> {
+  // Sets the default theme to light theme
+  let theme: HostTheme = "light";
+
+  // Check if Teams SDK is available and get the theme asynchronously
+  if (context.sdks?.microsoftTeams?.teamsJs?.app?.getContext) {
+    const teamsContext = await context.sdks.microsoftTeams.teamsJs.app.getContext();
+    theme = teamsContext.app?.appInfo?.theme as HostTheme;
+  }
+  return theme;
 }
